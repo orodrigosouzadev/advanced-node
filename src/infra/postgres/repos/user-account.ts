288dda0c4608c242bc2e3c6f7948b881fa0d391a
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-11-19 19:24:42 -0300

    feat: ensure PgUserAccountRepository returns account id

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -6,6 +6,7 @@ import { getRepository } from 'typeorm'
 type LoadParams = LoadUserAccountRepository.Params
 type LoadResult = LoadUserAccountRepository.Result
 type SaveParams = SaveFacebookAccountRepository.Params
+type SaveResult = SaveFacebookAccountRepository.Result
 
 export class PgUserAccountRepository implements LoadUserAccountRepository {
   async load (params: LoadParams): Promise<LoadResult> {
@@ -19,15 +20,18 @@ export class PgUserAccountRepository implements LoadUserAccountRepository {
     }
   }
 
-  async saveWithFacebook (params: SaveParams): Promise<void> {
+  async saveWithFacebook (params: SaveParams): Promise<SaveResult> {
     const pgUserRepo = getRepository(PgUser)
+    let id: string
     if (params.id === undefined) {
-      await pgUserRepo.save({
+      const pgUser = await pgUserRepo.save({
         email: params.email,
         name: params.name,
         facebookId: params.facebookId
       })
+      id = pgUser.id.toString()
     } else {
+      id = params.id
       await pgUserRepo.update({
         id: parseInt(params.id)
       }, {
@@ -35,5 +39,6 @@ export class PgUserAccountRepository implements LoadUserAccountRepository {
         facebookId: params.facebookId
       })
     }
+    return { id }
   }
 }
