4e2b9cf85521bdfc22fcda4f94d457d2de28af8b
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-11-24 11:37:02 -0300

    refactor: change FacebookAuthentication to return DTO instead of Entity

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -2,7 +2,6 @@ import { Controller } from '@/application/controllers'
 import { HttpResponse, ok, unauthorized } from '@/application/helpers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 import { FacebookAuthentication } from '@/domain/use-cases'
-import { AccessToken } from '@/domain/entities'
 
 type HttpRequest = { token: string }
 type Model = Error | { accessToken: string }
@@ -13,10 +12,12 @@ export class FacebookLoginController extends Controller {
   }
 
   async perform ({ token }: HttpRequest): Promise<HttpResponse<Model>> {
-    const accessToken = await this.facebookAuthentication({ token })
-    return accessToken instanceof AccessToken
-      ? ok({ accessToken: accessToken.value })
-      : unauthorized()
+    try {
+      const accessToken = await this.facebookAuthentication({ token })
+      return ok(accessToken)
+    } catch {
+      return unauthorized()
+    }
   }
 
   override buildValidators ({ token }: HttpRequest): Validator[] {
