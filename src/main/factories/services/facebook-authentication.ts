8c18b07ded13fdcf2e655a1d1d6789ddffb1d0af
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-11-24 09:45:05 -0300

    refactor: convert FacebookAtuehnticationUseCase into a function

--- src/main/factories/services/facebook-authentication.ts
+++ src/main/factories/services/facebook-authentication.ts
@@ -1,10 +1,10 @@
-import { FacebookAuthenticationService } from '@/data/services'
+import { setupFacebookAuthentication, FacebookAuthentication } from '@/data/services'
 import { makeFacebookApi } from '@/main/factories/apis'
 import { makeJwtTokenGenerator } from '@/main/factories/crypto'
 import { makePgUserAccountRepo } from '@/main/factories/repos'
 
-export const makeFacebookAuthenticationService = (): FacebookAuthenticationService => {
-  return new FacebookAuthenticationService(
+export const makeFacebookAuthenticationService = (): FacebookAuthentication => {
+  return setupFacebookAuthentication(
     makeFacebookApi(),
     makePgUserAccountRepo(),
     makeJwtTokenGenerator()
