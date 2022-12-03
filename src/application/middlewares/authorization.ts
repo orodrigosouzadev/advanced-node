c9901eda6528f7c2860d77a45cacd3a6b3953f17
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2022-12-03 13:56:06 -0300

    feat: ensure AuthenticationMiddleware returns 403 if authorization header was not provided

--- src/application/middlewares/authorization.ts
+++ src/application/middlewares/authorization.ts
@@ -1,12 +1,13 @@
 import { forbidden, HttpResponse, ok } from '@/application/helpers'
+import { Middleware } from '@/application/middlewares'
 import { RequiredStringValidator } from '@/application/validation'
 import { Authorize } from '@/domain/use-cases'
 
 type HttpRequest = { authorization: string }
 type Model = Error | { userId: string }
 
-export class AuthenticationMiddleware {
-  constructor (private readonly authorize: Authorize) {}
+export class AuthenticationMiddleware implements Middleware {
+  constructor (private readonly authorize: Authorize) { }
 
   async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
     if (!this.validate({ authorization })) return forbidden()
